c86a33032b6324953b88cab4cb2aedc2b9f2f8bf
LamUong <lam.uong2@mail.mcgill.ca>
2016-08-23 17:50:42 -0400

    update

--- GP.py
+++ GP.py
@@ -4,14 +4,19 @@ from sklearn.cross_validation import cross_val_score, KFold
 import math
 import numpy
 from sys import argv
+import matplotlib.pyplot as plt
+import numpy as np
 
+
+#Calculate Plane Equation given 3 coordinates
 def plane_Equation(Coor1,Coor2,Coor3):
 	v1 = (Coor3[0]-Coor1[0],Coor3[1]-Coor1[1],Coor3[2]-Coor1[2])
 	v2 = (Coor2[0]-Coor1[0],Coor2[1]-Coor1[1],Coor2[2]-Coor1[2])
 	crossv1v2 = (v1[1]*v2[2]-v1[2]*v2[1],v1[2]*v2[0]-v1[0]*v2[2],v1[0]*v2[1]-v1[1]*v2[0])
 	z = crossv1v2[0]*Coor1[0]+crossv1v2[1]*Coor1[1]+crossv1v2[2]*Coor1[2]
 	return(crossv1v2[0],crossv1v2[1],crossv1v2[2],z)
-	
+
+#Calculate Dihedral Angle given 4 coordinates
 def dihedral_Angle(Coor1,Coor2,Coor3,Coor4):
 	Equ1 = plane_Equation(Coor1,Coor2,Coor3)
 	Equ2 = plane_Equation(Coor2,Coor3,Coor4)
@@ -24,6 +29,7 @@ def dihedral_Angle(Coor1,Coor2,Coor3,Coor4):
 	angleDegree = math.degrees(angle)
 	return angleDegree
 
+#Caculating Cosine of angle at molA point
 def cosine_from_coordinates(bisector, molA, molB):
 	distanceA=float(distance_2_coordinates(bisector,molA))
  	distanceB=float(distance_2_coordinates(molA,molB))
@@ -31,6 +37,7 @@ def cosine_from_coordinates(bisector, molA, molB):
  	cos = ((distanceB**2)+(distanceA**2)-(distanceC**2))/(2*(distanceA*distanceB))
  	return cos
 
+#Searching for the coordinate of Angle Bisector 
 def recursive_search(Ratio,Coor2,Coor3,OriginCoor2,OriginCoo3):
  	Mid = (float(Coor2[0]+Coor3[0])/2,float(Coor2[1]+Coor3[1])/2,float(Coor2[2]+Coor3[2])/2)
  	D12 = distance_2_coordinates(OriginCoor2,Mid)
@@ -44,6 +51,7 @@ def recursive_search(Ratio,Coor2,Coor3,OriginCoor2,OriginCoo3):
  	elif CalculatedRatio>Ratio:
  		return recursive_search(Ratio,Coor2,Mid,OriginCoor2,OriginCoo3)
 
+#Searching for the coordinate of Angle Bisector 
 def angle_bisector_coordinates(Coor1,Coor2,Coor3):
   	#Coor1 is the apex
  	D12 = distance_2_coordinates(Coor1,Coor2)
@@ -62,10 +70,20 @@ def average_between_distances(Listofdistances):
 def symmetric_differences_of_2_distances(Distanceij,Distanceik):
 	return ((Distanceij-Distanceik)**2)
 
-def get_Parameters_for_Gaussian(data):
-	ToReturn = []
-	for dimer in data:
-
+def get_parameters_for_Gaussian(data,energy):
+	x_train = []
+	y_train = []
+	x_test = []
+	y_test = []
+	number = 0
+	count1= 3000
+	count2=3000
+	count3=3000
+	count4=3000
+	count5=3000
+	count6=3000
+	for dimer, ene in zip(data, energy):
+		number+= 1
 		distance12 = distance_2_coordinates(dimer[0],dimer[1])
 		distance13 = distance_2_coordinates(dimer[0],dimer[2])
 		if distance12 < distance13:
@@ -97,10 +115,51 @@ def get_Parameters_for_Gaussian(data):
 		diheXbO4O1Xa = dihedral_Angle(Xb,dimer[3],dimer[0],Xa)
 		DiheHmXaO1O4 = dihedral_Angle(Hm,Xa,dimer[0],dimer[3])
 		DiheHnXbO4O1 = dihedral_Angle(Hn,Xb,dimer[3],dimer[0])
-		ToReturn.append([average1213, dif1213, distance23, average4546, dif4546, distance56, 
-			distance2Oxygens, cosXaO1O4, cosXbO4O1, diheXbO4O1Xa, DiheHmXaO1O4, DiheHnXbO4O1]) 
-	
-	return ToReturn
+		if number <200:
+			x_test.append([average1213, dif1213, distance23, average4546, dif4546, distance56, 
+				distance2Oxygens, cosXaO1O4, cosXbO4O1, diheXbO4O1Xa, DiheHmXaO1O4, DiheHnXbO4O1]) 
+			y_test.append(ene)
+		else:
+			if distance2Oxygens <=3 and count1>=0:
+				count1-=1 
+				x_train.append([average1213, dif1213, distance23, average4546, dif4546, distance56, 
+					distance2Oxygens, cosXaO1O4, cosXbO4O1, diheXbO4O1Xa, DiheHmXaO1O4, DiheHnXbO4O1]) 
+				y_train.append(ene)
+			elif distance2Oxygens >3 and distance2Oxygens<=4 and count2>=0:
+				count2-=1
+				x_train.append([average1213, dif1213, distance23, average4546, dif4546, distance56, 
+					distance2Oxygens, cosXaO1O4, cosXbO4O1, diheXbO4O1Xa, DiheHmXaO1O4, DiheHnXbO4O1]) 
+				y_train.append(ene)
+			elif distance2Oxygens >4 and distance2Oxygens<=5 and count3>=0:
+				count3-=1
+				x_train.append([average1213, dif1213, distance23, average4546, dif4546, distance56, 
+					distance2Oxygens, cosXaO1O4, cosXbO4O1, diheXbO4O1Xa, DiheHmXaO1O4, DiheHnXbO4O1]) 
+				y_train.append(ene)
+			elif distance2Oxygens >5 and distance2Oxygens<=6 and  count4>=0:
+				count4-=1
+				x_train.append([average1213, dif1213, distance23, average4546, dif4546, distance56, 
+					distance2Oxygens, cosXaO1O4, cosXbO4O1, diheXbO4O1Xa, DiheHmXaO1O4, DiheHnXbO4O1]) 
+				y_train.append(ene)
+			elif distance2Oxygens >6 and distance2Oxygens<=7 and  count5>=0: 
+				count5-=1
+				x_train.append([average1213, dif1213, distance23, average4546, dif4546, distance56, 
+					distance2Oxygens, cosXaO1O4, cosXbO4O1, diheXbO4O1Xa, DiheHmXaO1O4, DiheHnXbO4O1]) 
+				y_train.append(ene)
+			elif distance2Oxygens >7 and count6>=0:
+				count6-=1
+				x_train.append([average1213, dif1213, distance23, average4546, dif4546, distance56, 
+					distance2Oxygens, cosXaO1O4, cosXbO4O1, diheXbO4O1Xa, DiheHmXaO1O4, DiheHnXbO4O1]) 
+				y_train.append(ene)
+	print("a")	
+
+	print count1
+	print count2
+	print count3
+	print count4
+	print count5
+	print count6
+	print("a")	
+	return x_train, y_train, x_test, y_test 
 
 def load_data(): 
 	Data_array = []
@@ -121,41 +180,76 @@ def load_data():
 					Coordinates = (float(Splitted_Line[1]),float(Splitted_Line[2]),float(Splitted_Line[3]))
 					NewSubEnergyArray.append(Coordinates)
 			number+=1
-	Parameters = get_Parameters_for_Gaussian(Data_array)
-	return Parameters,Energy_Array
+	x_train, y_train, x_test, y_test = get_parameters_for_Gaussian(Data_array, Energy_Array)
+	return x_train, y_train, x_test, y_test
 
 def learning(regression,correlation,start):
 	print("Loading data and generating descriptors")
-	Dataarray,Energyarray = load_data()
-	X_train, y_train = numpy.asarray(Dataarray[start:]) , numpy.asarray(Energyarray[start:])
+	x_train, y_train, x_test, y_test = load_data()
+	print len(x_train)
+	print len(y_train)
+
+	X_train, y_train = numpy.asarray(x_train) , numpy.asarray(y_train)
+	X_val, y_val = numpy.asarray(x_test) , numpy.asarray(y_test)
+
 	print("Generating GP")
 	gp = GaussianProcess(corr=correlation, normalize=True, regr=regression, thetaL=1e-2, thetaU=1.0)
 	gp.fit(X_train, y_train)
-	return gp, Dataarray, Energyarray
+	return gp, x_train, y_train, x_test, y_test
 
-def predict(x, gp, Dataarray, Energyarray):
-	f2, MSE2 = gp.predict([Dataarray[x]], eval_MSE=True)
-	a = f2[0]
-	b = Energyarray[x]
-	return math.fabs(a-b)
+def predict(index, gp, x_test, y_test):
+	f2, MSE = gp.predict([x_test[index]], eval_MSE=True)
+	predicted_energy = f2[0]
+	validation_energy = y_test[index]
+	sigma = np.sqrt(MSE)
+	absolute_differences = math.fabs(predicted_energy-validation_energy)
+	return sigma, predicted_energy, validation_energy, absolute_differences
+
+def plot(x,y,e,truey):
+	x = np.array(x)
+	y = np.array(y) 
+	e = np.array(e)
+	truey = np.array(truey)
+	plt.plot(x, truey, 'ro')
+	plt.errorbar(x, y, e, linestyle='None', marker='^')
+	plt.savefig('all.png')
 
 def main():
+	'''
 	regr = ['constant', 'linear', 'quadratic']
 	corr = ['absolute_exponential', 'squared_exponential','generalized_exponential', 'cubic', 'linear']
 	startlist = [1000,2000,3000,4000,5000,5200,5400,5600]
 	nugget = 2.2204460492503131e-15
-
-
+	'''
 	regression = 'quadratic'
 	correlation = 'squared_exponential'
-	start = 5000
+	start = 100
 
-	
-	gp, Dataarray, Energyarray = learning(regression,correlation,start)
-	sum = 0
+	gp, x_train, y_train, x_test, y_test = learning(regression,correlation,start)
+
+	x = []
+	y = []
+	e = []
+	truey = []
 	print("Calculating MSE")
-	for x in range(0,1000,1):
-		sum += predict(x,gp,Dataarray,Energyarray)
-	print ("MSE of "+regression+" and "+ correlation+ " start at " +  str(start) +" is "+str(sum/1000))
 	
+	MSE =0
+	number =0
+	for index in range(0,199,1):
+		number+=1 
+		print index
+		sigma, predicted_energy, validation_energy, absolute_differences = predict(index, gp, x_test, y_test)
+
+		MSE += absolute_differences**2
+		
+		x.append(x_test[index][6])
+		y.append(predicted_energy)
+		e.append(2*sigma)
+		truey.append(validation_energy)
+
+	MSE = MSE/100
+	print MSE
+
+	plot(x,y,e,truey)
+
 main()
